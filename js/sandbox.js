20d6f4f7c36aea04b43b60b5115079cc8f68223d
daana <126dpelaezayala@frhsd.com>
2025-01-09 14:03:26 -0500

    message

---
+++ js/sandbox.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#myImage").hover(function(){
+      $(this).css("src", "img/Bartholomew Bear heart.jpg");
+      }, function(){
+      $(this).css("src", "img/Bartholomew Bear pumpkin.jpg");
+    });
+  });
+  
