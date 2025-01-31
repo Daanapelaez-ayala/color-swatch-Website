28fa1d2d034fcd7377ecddb893aa3dde422c5280
daana <126dpelaezayala@frhsd.com>
2025-01-31 11:27:47 -0500

    Message

--- js/sandbox.js
+++ js/sandbox.js
@@ -9,5 +9,22 @@
       $(".overview").attr("src","img/bartholomew bear strawberry.jpg");
     });
   
+    // Get the main image element and all thumbnail images
+const mainImage = document.getElementById('mainImage');
+const thumbnails = document.querySelectorAll('.thumbnail');
+
+// Loop through each thumbnail image and add event listeners for hover
+thumbnails.forEach(thumbnail => {
+  // When mouse enters a thumbnail, change the main image
+  thumbnail.addEventListener('mouseover', function() {
+    mainImage.src = thumbnail.getAttribute('data-image');
+  });
+
+  // When mouse leaves the thumbnail, reset the main image to the default one
+  thumbnail.addEventListener('mouseout', function() {
+    mainImage.src = "img/Bartholomew Bear.jpg"; // reset to original image
+  });
+});
+
 
     
